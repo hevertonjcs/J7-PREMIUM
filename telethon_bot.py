e2f13e49888e30974fc99b3f011ec87be7719f69
hevertonjcs <heverton167cristaldo@gmail.com>
2025-07-30 19:59:43 -0300

    Update telethon_bot.py

--- telethon_bot.py
+++ telethon_bot.py
@@ -1,4 +1,15 @@
-
 async def comprar_no_fornecedor(produto_nome):
-    # Aqui entraria o código real do Telethon (automatizando @storevisionario_bot)
-    return f"Simulação de conteúdo do produto: {produto_nome} - Código: XXXX-XXXX-XXXX"
+    # Simulação de retorno com campos estruturados
+    return {
+        "cartao": "4258502278889163",
+        "validade": "12/2028",
+        "cvv": "113",
+        "bandeira": "visa",
+        "nivel": "infinite",
+        "tipo": "credit",
+        "banco": "banco santander s.a.",
+        "pais": "brasil",
+        "nome": "ANDRE LUIS BARDT",
+        "cpf": "05225724990",
+        "nascimento": "29/04/1989"
+    }
